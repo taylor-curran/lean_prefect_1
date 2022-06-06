1ea641af424f7ac3471b935cbebfb5de5f62f724
Taylor Curran <taylorcurranagtc@gmail.com>
2022-06-06 16:04:15 -0400

    Adding hello world task and flow

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+import prefect
+from prefect import task, Flow
+
+@task
+def hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello world!")
+
+with Flow("hello-flow") as flow:
+    hello_task()
+
+if __name__ == "__main__":
+    flow.run()
